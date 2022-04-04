df62ad8912c0f28d993331bf73d72da10ca45e34
Joshan Rai <pradheon@gmail.com>
2022-04-04 14:44:19 -0700

    Initial Commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Joshan Rai on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
